def2115a3729c0a48a3a17f602cb7015fa9b640a
NorbertKny <92684323+NorbertKny@users.noreply.github.com>
2021-11-22 14:00:49 +0100

    Add files via upload

---
+++ ttestt/src/ttestt/Ttestt.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ttestt;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author norbert.roland.kny
+ */
+public class Ttestt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        //int poculoh = sc.nextInt();
+        int num1, num2, num3, num4;
+        //for (int i=1; i< poculoh+1; i++){
+        num1 = sc.nextInt(); num2 = sc.nextInt(); num3 = sc.nextInt(); num4 = sc.nextInt();
+        System.out.println(num1+" "+num2+" "+num3+" "+num4);
+        //}
+        //System.out.println(num1+" "+num2+" "+num3+" "+num4);
+    }
+    
+}
